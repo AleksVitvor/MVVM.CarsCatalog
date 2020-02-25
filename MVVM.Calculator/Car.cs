14859c449c0dc093f47e87786ec0f7d1510adf67
lesha25012001 <lesha25012001@outlook.com>
2020-02-25 17:26:29 +0300

    DB connected

--- MVVM.Calculator/Car.cs
+++ MVVM.Calculator/Car.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+﻿using System.ComponentModel;
 using System.Data.SqlClient;
-using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MVVM.CarsCatalog
 {
--- MVVM.Calculator/CarViewModel.cs
+++ MVVM.Calculator/CarViewModel.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
+﻿using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Data.SqlClient;
-using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MVVM.CarsCatalog
 {
